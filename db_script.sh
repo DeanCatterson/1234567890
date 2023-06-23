2372b866b3d59da0e03f2ce65161cbd15ae7d57b
Dean Catterson <dean.catterson@digital.homeoffice.gov.uk>
2023-06-23 22:10:24 +0100

    Add skeleton of db config

---
+++ db_script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#This script contains the commands to set up our database, user and data
+
--- src/main/java/config/Config.java
+++ src/main/java/config/Config.java
@@ -1,7 +1,34 @@
 package config;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Config {
 
 	//	This file will contain all the java related config neccesary to connect to the datastore which
 	//	will contain the weather data
+
+	public static void connectToDb() {
+		Connection connection = null;
+
+		try {
+			String url = "jdbc:postgresql://localhost:8001/weatherdatabase";
+			String username = "username";
+			String password = "password";
+			connection = DriverManager.getConnection(url, username, password);
+
+			// TODO: Connection successful, hit queries in WeatherRepository from here
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 }
