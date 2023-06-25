5246e7d6f3ce8cf34f3237d5584c205ce7f07fbb
Dean Catterson <dean.catterson@c02f71q4ml85.fritz.box>
2023-06-25 14:53:46 +0100

    Add a TODO

--- src/main/java/config/Config.java
+++ src/main/java/config/Config.java
@@ -8,6 +8,8 @@ public class Config {
 
 	//	This file will contain all the java related config neccesary to connect to the datastore which
 	//	will contain the weather data\
+
+	//TODO: get h2 in memory db working within app
 	public static void connectToH2Db() throws Exception {
 		Class.forName("org.h2.Driver").newInstance();
 		Connection con = DriverManager.getConnection("jdbc:h2:" + "./Database.my", "root", "password");
